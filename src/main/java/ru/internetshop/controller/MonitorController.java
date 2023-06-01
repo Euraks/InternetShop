a1bafd233464b7fcbf59a6919513c68bf5e0101b
Yaroslav Agapov <euraks@yandex.ru>
2023-06-01 22:29:53 +0500

    feat: Add MonitorController

--- src/main/java/ru/internetshop/controller/MonitorController.java
+++ src/main/java/ru/internetshop/controller/MonitorController.java
@@ -7,26 +7,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.internetshop.model.HardDrive;
 import ru.internetshop.model.Monitor;
-import ru.internetshop.model.NoteBook;
-import ru.internetshop.model.PersonalComputer;
-import ru.internetshop.repository.HardDriveRepository;
 import ru.internetshop.repository.MonitorRepository;
-import ru.internetshop.repository.NoteBookRepository;
-import ru.internetshop.repository.PersonalComputerRepository;
 
 @RestController
 @RequestMapping("/v01")
-public class ShopController {
+public class MonitorController {
 
     @Autowired
-    HardDriveRepository hardDriveRepository;
+    MonitorRepository monitorRepository;
 
-    @PostMapping(value = "/HardDrives")
-    public ResponseEntity<?> create(@RequestBody HardDrive hardDrive) {
-        hardDriveRepository.save(hardDrive);
+    @PostMapping(value = "/Monitors")
+    public ResponseEntity<?> create(@RequestBody Monitor monitor) {
+        monitorRepository.save(monitor);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
-
 }
