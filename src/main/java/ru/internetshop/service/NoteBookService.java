18541f75142fd0499ac4978a697b68e9b320babd
Yaroslav Agapov <euraks@yandex.ru>
2023-06-01 23:36:26 +0500

    feat: Add NoteBookService

---
+++ src/main/java/ru/internetshop/service/NoteBookService.java
@@ -0,0 +1,40 @@
+package ru.internetshop.service;
+
+import ru.internetshop.model.NoteBook;
+import ru.internetshop.model.PersonalComputer;
+
+import java.util.List;
+
+public interface NoteBookService {
+    /**
+     * Create New NoteBook
+     *
+     * @param noteBook - NoteBook to create
+     */
+    void create(NoteBook noteBook);
+
+    /**
+     * Return all NoteBooks
+     *
+     * @return list NoteBook
+     */
+    List<NoteBook> readAll();
+
+    /**
+     * Return NoteBook for ID
+     *
+     * @param id - ID NoteBook
+     * @return - NoteBook for ID
+     */
+    PersonalComputer read(int id);
+
+    /**
+     * Update NoteBook for ID
+     *
+     * @param noteBook - NoteBook where to update the data
+     * @param id     - id NoteBook to update
+     * @return - true if data have been updated, else false
+     */
+    boolean update(NoteBook noteBook, int id);
+
+}
