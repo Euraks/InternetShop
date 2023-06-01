d06e393c8afc0aa0cecf9344b549160ce3d45145
Yaroslav Agapov <euraks@yandex.ru>
2023-06-01 22:58:36 +0500

    feat: Add method PersonalComputersController.read()

--- src/main/java/ru/internetshop/controller/PersonalComputersController.java
+++ src/main/java/ru/internetshop/controller/PersonalComputersController.java
@@ -3,12 +3,11 @@ package ru.internetshop.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.internetshop.model.PersonalComputer;
 import ru.internetshop.repository.PersonalComputerRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v01")
@@ -21,4 +20,15 @@ public class PersonalComputersController {
         personalComputerRepository.save(personalComputer);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
+
+    @GetMapping(value = "/PersonalComputers")
+    public ResponseEntity<List<PersonalComputer>> read() {
+        final List<PersonalComputer> personalComputers = personalComputerRepository.findAll();
+
+        return !personalComputers.isEmpty()
+                ? new ResponseEntity<>(personalComputers, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+
 }
