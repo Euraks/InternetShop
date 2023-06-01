b3e3f19bc8972b27455a463a3a92b5d4e80fa73d
Yaroslav Agapov <euraks@yandex.ru>
2023-06-01 23:00:26 +0500

    feat: Add method MonitorController.read()

--- src/main/java/ru/internetshop/controller/MonitorController.java
+++ src/main/java/ru/internetshop/controller/MonitorController.java
@@ -3,12 +3,12 @@ package ru.internetshop.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.internetshop.model.Monitor;
+import ru.internetshop.model.NoteBook;
 import ru.internetshop.repository.MonitorRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v01")
@@ -21,5 +21,14 @@ public class MonitorController {
     public ResponseEntity<?> create(@RequestBody Monitor monitor) {
         monitorRepository.save(monitor);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/Monitors")
+    public ResponseEntity<List<Monitor>> read() {
+        final List<Monitor> personalComputers = monitorRepository.findAll();
+
+        return !personalComputers.isEmpty()
+                ? new ResponseEntity<>(personalComputers, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
