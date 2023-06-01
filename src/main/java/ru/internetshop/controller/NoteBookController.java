b37bb5b9899ef99112371c1d9ad32e0392d60ecb
Yaroslav Agapov <euraks@yandex.ru>
2023-06-01 22:59:29 +0500

    feat: Add method NoteBookController.read()

--- src/main/java/ru/internetshop/controller/NoteBookController.java
+++ src/main/java/ru/internetshop/controller/NoteBookController.java
@@ -3,12 +3,12 @@ package ru.internetshop.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.internetshop.model.NoteBook;
+import ru.internetshop.model.PersonalComputer;
 import ru.internetshop.repository.NoteBookRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v01")
@@ -21,5 +21,14 @@ public class NoteBookController {
     public ResponseEntity<?> create(@RequestBody NoteBook noteBook) {
         noteBookRepository.save(noteBook);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/NoteBooks")
+    public ResponseEntity<List<NoteBook>> read() {
+        final List<NoteBook> personalComputers = noteBookRepository.findAll();
+
+        return !personalComputers.isEmpty()
+                ? new ResponseEntity<>(personalComputers, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
